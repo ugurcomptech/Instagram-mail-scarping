65a8f939e39f9d721dcd053d988aba77a960da49
Uğur <133202238+ugurcomptech@users.noreply.github.com>
2023-08-10 12:47:49 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,27 +1,18 @@
 import time
 import pandas as pd
 import re
-import argparse
 import requests
 from bs4 import BeautifulSoup
 from validate_email_address import validate_email
 
-# Argümanları tanımla
-parser = argparse.ArgumentParser(description="Google'da belirli bir kelimeyi belirttiğiniz web sitesinde hedefleyen e-posta adreslerini bulur.")
-parser.add_argument("-s", "--site", required=True, help="E-posta adresleri çekilecek web sitesini belirtin.")
-parser.add_argument("-k", "--keyword", required=True, help="Aranacak kelimeyi belirtin.")
-parser.add_argument("-n", "--num_emails", type=int, required=True, help="Kaç adet e-posta adresi çekileceğini belirtin.")
-args = parser.parse_args()
+# Kullanıcıdan aranacak kelimeyi alın
+aranacak_kelime = input("Aramak istediğiniz kelimeyi girin: ")
 
-# Kullanıcının belirttiği siteyi ve kelimeyi al
-selected_site = args.site
-aranacak_kelime = args.keyword
-
-# Kullanıcının belirttiği e-posta adeti sayısını al
-eposta_adresi_sayisi = args.num_emails
+# Kaç adet e-posta adresi istendiğini kullanıcıdan alın
+eposta_adresi_sayisi = int(input("Kaç adet e-posta adresi istediğinizi girin: "))
 
 # Arama sorgusu oluşturun
-arama_kelimesi = f'{aranacak_kelime} "@{selected_site}" site:{selected_site}'
+arama_kelimesi = f'{aranacak_kelime} "@gmail.com" site:instagram.com'
 
 # E-posta adreslerini tutmak için bir liste oluşturun
 eposta_listesi = []
@@ -55,6 +46,6 @@ for i in range(10):  # İlk 10 sayfa için döngü oluşturduk
 
 # E-posta adreslerini bir Excel dosyasına kaydedin
 df = pd.DataFrame({'E-posta Adresi': eposta_listesi})
-df.to_excel(f'eposta_adresleri_{selected_site}.xlsx', index=False)
+df.to_excel('eposta_adresleri.xlsx', index=False)
 
 print("E-posta adresleri başarıyla kaydedildi.")
